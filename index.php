3965a6481843bc399801402cfe2fe29277282098
Kevin-Diez <kevin.diez.pro@gmail.com>
2017-01-23 16:52:44 +0100

    V0.13 Ajout fonction rechercher Ville
    Seul la recherche sur l'input ville fonctionne

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
         
     </head>
     
-    <body class="reset maxsize flex-column">
+    <body class="abs reset maxsize flex-column">
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
@@ -30,59 +30,22 @@
         
         <main class="maxsize flexible flex-column-center">
         
-            <section id="main_container" class="flex-wrap">
+            <section id="main_container" class="flex-column">
                 
                 <header class="maxwidth txtcenter">
                     <h1>Trouve ta ville</h1>
                 </header>
                 
-                <form class="maxwidth txtcenter">
+                <form class="maxwidth txtcenter" method="post" action="index.php">
                     <p><input type="text" name="ville" placeholder="Ville"/></p>
                     <p><input type="text" name="dept" placeholder="Département"/></p>
                     <p><input type="text" name="region" placeholder="Région"/></p>
-                    <p><input type="submit" name="valider" value="Chercher"/></p>
+                    <p><input type="submit" name="recherche" value="Chercher"/></p>
                 </form>    
                 
                 <div id="result" class="maxwidth">
                     <?php
-
-                    $dsn='mongodb://192.168.1.42:27017';
-                    $dbname = 'geo_france';
-                    $collname = 'villes';
-
-                    try {
-
-                        $mgc = new MongoDB\Driver\Manager($dsn);
-
-                        $options = [
-                            //'projection' => ['_id' => 1, 'nom' => 1],
-                            'limit' => 10
-                        ];
-
-                        $query = new MongoDB\Driver\Query(
-                            ['nom' => ['$exists' => true]], // population > 2000000
-                            $options
-                        );
-
-                        $curs = $mgc->executeQuery(
-                            'geo_france.villes', // la collection visée
-                            $query               // la requête
-                        );
-                        
-                        echo "<ul>";
-                        
-                        foreach($curs as $doc) {
-                            echo "<li>";
-                            echo $doc->_id." ".$doc->nom;
-                            echo "</li>";
-                        }
-                        
-                        echo "</ul>";
-
-
-                    } catch (Exception $e) {
-                      echo "exception interceptée :".$e->getMessage();
-                    }
+                        require_once("php/afficherVille.php");
                     ?>
                 </div>
                 
--- php/afficherVille.php
+++ php/afficherVille.php
@@ -3,35 +3,42 @@
 require_once("php/db.php");
 
 try {
-
+    
+    if(isset($_POST["recherche"])&&$_POST["recherche"]=="Chercher") {
+        
+    $ville = trim($_POST["ville"]);
+    $dept = trim($_POST["dept"]);
+    $region = trim($_POST["region"]);
+        
     $options = [
-        //'projection' => ['_id' => 1, 'nom' => 1],
+        'sort' => ['pop' => -1],
         'limit' => 10
     ];
+        
+    $filter = ['nom' => new MongoDB\BSON\Regex("^$ville$","i")];
 
-    $query = new MongoDB\Driver\Query(
-        ['nom' => ['$exists' => true]], // population > 2000000
-        $options
-    );
+    $query = new MongoDB\Driver\Query($filter,$options);
 
-    $curs = $mgc->executeQuery(
-        'geo_france.villes', // la collection visée
-        $query               // la requête
-    );
+    $curs = $mgc->executeQuery('geo_france.villes', $query);
+        
+    $cursArray = $curs->toArray();
+        
+    if(count($cursArray) == 1) {
+        
+        echo "<ul>";
 
-    echo "<ul>";
+        foreach($cursArray as $key => $doc) {
+            echo "<li>";
+            echo $key." : ".$doc->_id." ".$doc->nom;
+            echo "</li>";
+        }
 
-    foreach($curs as $doc) {
-        echo "<li>";
-        echo $doc->_id." ".$doc->nom;
-        echo "</li>";
+        echo "</ul>";
+
+        }
     }
 
-    echo "</ul>";
-
-
-} catch (Exception $e) {
-    echo "exception interceptée :".$e->getMessage();
 }
+catch (Exception $e) { echo "exception interceptée :".$e->getMessage();}
 
 ?>
